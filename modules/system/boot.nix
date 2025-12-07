2c6484692be9ff64aa2df8581814a3d7e61d2eb6
lov3g00d <zamkovoy99@gmail.com>
2025-12-07 15:06:01 -0500

    Added AMD performance enhancement params

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -8,6 +8,17 @@
   # Enable latest kernel for better hardware support
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  # Firmware + Microcode
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.amd.updateMicrocode = true;
+
+  # Kernel parameters for Ryzen stability
+  boot.kernelParams = [
+    "amd_pstate=disable"
+    "processor.max_cstate=5"
+    "idle=nomwait"
+  ];
+
   # Kernel parameters for better performance
   boot.kernel.sysctl = {
     "vm.swappiness" = 10;
